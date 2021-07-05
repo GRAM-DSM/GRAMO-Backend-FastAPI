d3deff529a3fa3b2d7b92d8054dd1290c036b0ab
junkiYoon <juneki4933@gmail.com>
2021-07-05 15:23:31 +0900

    Update main
    host 0.0.0.0

--- project/main.py
+++ project/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 
+import uvicorn
+
 from project.apps import auth, notice
 
-app = FastAPI()
 
-app.include_router(auth.router)
-app.include_router(notice.router)
+if __name__ == '__main__':
+    app = FastAPI()
+
+    app.include_router(auth.router)
+    app.include_router(notice.router)
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
