a677ab0eae428a3a56304362a6138622956f2871
junkiYoon <juneki4933@gmail.com>
2021-08-17 09:29:27 +0900

    Update sqlalchemy session
    create session_scope, and call it before use session

--- project/apps/auth.py
+++ project/apps/auth.py
@@ -10,7 +10,7 @@ import random
 
 from project.core.schemas.auth import Signup, Login, SendEmail, CheckCode
 
-from project.core.models import session, Redis
+from project.core.models import session_scope, Redis
 from project.core.models.user import User_tbl
 
 from project.utils import create, delete
@@ -26,36 +26,38 @@ router = APIRouter()
 
 @router.post("/signup", status_code=status.HTTP_201_CREATED, tags=["auth"])
 async def signup(body: Signup):
-    is_user(session=session, email=body.email)
+    with session_scope() as session:
+        is_user(session=session, email=body.email)
 
-    create(
-        cls=User_tbl,
-        session=session,
-        email=body.email,
-        password=bcrypt.hashpw(body.password.encode("utf-8"), bcrypt.gensalt()),
-        name=body.name,
-        major=body.major,
-        email_status=1
-    )
+        create(
+            cls=User_tbl,
+            session=session,
+            email=body.email,
+            password=bcrypt.hashpw(body.password.encode("utf-8"), bcrypt.gensalt()),
+            name=body.name,
+            major=body.major,
+            email_status=1
+        )
 
-    return {
-        "message": "success"
-    }
+        return {
+            "message": "success"
+        }
 
 
 @router.post("/sendemail", status_code=status.HTTP_200_OK, tags=["auth"])
 async def send_email(body: SendEmail):
-    is_user(session=session, email=body.email)
+    with session_scope() as session:
+        is_user(session=session, email=body.email)
 
-    code = f"{random.randint(111111, 999999):04d}"
+        code = f"{random.randint(111111, 999999):04d}"
 
-    await send_code(code=code, address=body.email)
+        await send_code(code=code, address=body.email)
 
-    Redis.setex(name=body.email, value=code, time=timedelta(minutes=3))
+        Redis.setex(name=body.email, value=code, time=timedelta(minutes=3))
 
-    return {
-               "message": "success"
-           }
+        return {
+                   "message": "success"
+               }
 
 
 @router.post("/checkcode", status_code=status.HTTP_200_OK, tags=["auth"])
@@ -69,34 +71,35 @@ async def authenticate_email_code(body: CheckCode):
 
 @router.post("/auth", status_code=status.HTTP_200_OK, tags=["auth"])
 async def login(body: Login, authorize: AuthJWT = Depends()):
-    user = user_authentication(session=session, email=body.email, password=body.password)
+    with session_scope() as session:
+        user = user_authentication(session=session, email=body.email, password=body.password)
 
-    set_user_fcm_token(session=session, token=body.token, email=body.email)
+        set_user_fcm_token(session=session, token=body.token, email=body.email)
 
-    access_expires_delta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    refresh_expires_delta = timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
+        access_expires_delta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        refresh_expires_delta = timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
 
-    access_token = authorize.create_access_token(
-        subject=body.email,
-        algorithm=ALGORITHM,
-        expires_time=access_expires_delta
-    )
-    refresh_token = authorize.create_refresh_token(
-        subject=body.email,
-        algorithm=ALGORITHM,
-        expires_time=refresh_expires_delta
-    )
+        access_token = authorize.create_access_token(
+            subject=body.email,
+            algorithm=ALGORITHM,
+            expires_time=access_expires_delta
+        )
+        refresh_token = authorize.create_refresh_token(
+            subject=body.email,
+            algorithm=ALGORITHM,
+            expires_time=refresh_expires_delta
+        )
 
-    Redis.setex(name=body.email,
-                value=refresh_token,
-                time=refresh_expires_delta)
+        Redis.setex(name=body.email,
+                    value=refresh_token,
+                    time=refresh_expires_delta)
 
-    return {
-        "name": user.name,
-        "major": user.major,
-        "access_token": access_token,
-        "refresh_token": refresh_token
-    }
+        return {
+            "name": user.name,
+            "major": user.major,
+            "access_token": access_token,
+            "refresh_token": refresh_token
+        }
 
 
 @router.get("/auth", status_code=status.HTTP_201_CREATED, tags=["auth"])
@@ -135,12 +138,13 @@ async def logout(authorize: AuthJWT = Depends()):
 
 @router.delete("/withdrawal", status_code=status.HTTP_200_OK, tags=["auth"])
 async def withdrawal(authorize: AuthJWT = Depends()):
-    token_check(authorize=authorize, type="access")
+    with session_scope() as session:
+        token_check(authorize=authorize, type="access")
 
-    email = authorize.get_jwt_subject()
-    user = is_user(session=session, email=email, return_it=True)
-    delete(session=session, del_thing=user)
+        email = authorize.get_jwt_subject()
+        user = is_user(session=session, email=email, return_it=True)
+        delete(session=session, del_thing=user)
 
-    return {
-        "message": "success"
-    }
+        return {
+            "message": "success"
+        }
--- project/apps/notice.py
+++ project/apps/notice.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 from project.core.schemas.notice import CreateNotice
 
-from project.core.models import session
+from project.core.models import session_scope
 from project.core.models.notice import Notice_tbl
 
 from project.utils import create, delete
@@ -21,77 +21,81 @@ router = APIRouter()
 
 @router.post("/notice", status_code=status.HTTP_201_CREATED, tags=["notice"])
 async def create_notice(body: CreateNotice, authorize: AuthJWT = Depends()):
-    token_check(authorize=authorize, type="access")
+    with session_scope() as session:
+        token_check(authorize=authorize, type="access")
 
-    user_email = authorize.get_jwt_subject()
+        user_email = authorize.get_jwt_subject()
 
-    create(
-        cls=Notice_tbl,
-        session=session,
-        title=body.title,
-        content=body.content,
-        user_email=user_email,
-        created_at=datetime.now(timezone("Asia/Seoul")).strftime("%Y-%m-%d %H:%M:%S")
-    )
+        create(
+            cls=Notice_tbl,
+            session=session,
+            title=body.title,
+            content=body.content,
+            user_email=user_email,
+            created_at=datetime.now(timezone("Asia/Seoul")).strftime("%Y-%m-%d %H:%M:%S")
+        )
 
-    tokens = get_user_fcm_tokens(session=session, email=user_email)
-    send_message(tokens=tokens, title="새로운 공지사항", body=body.title)
+        tokens = get_user_fcm_tokens(session=session, email=user_email)
+        send_message(tokens=tokens, title="새로운 공지사항", body=body.title)
 
-    return {
-        "message": "success"
-    }
+        return {
+            "message": "success"
+        }
 
 
 @router.get("/notice/list/{page}", status_code=status.HTTP_200_OK, tags=["notice"])
 async def get_notices(page: int, authorize: AuthJWT = Depends()):
-    token_check(authorize=authorize, type="access")
+    with session_scope() as session:
+        token_check(authorize=authorize, type="access")
 
-    limit = 10
-    offset = page * limit
+        limit = 10
+        offset = page * limit
 
-    notice_list = get_notice_list(session=session, limit=limit, offset=offset)
-    is_next = is_next_notice(session=session, limit=limit, offset=offset)
+        notice_list = get_notice_list(session=session, limit=limit, offset=offset)
+        is_next = is_next_notice(session=session, limit=limit, offset=offset)
 
-    return {
-       "notice": [{
-           "id": notice.id,
-           "title": notice.title,
-           "content": notice.content,
-           "user_name": user.name,
-           "created_at": str(notice.created_at)
-       } for notice, user in notice_list],
-       "next_page": is_next
-    }
+        return {
+           "notice": [{
+               "id": notice.id,
+               "title": notice.title,
+               "content": notice.content,
+               "user_name": user.name,
+               "created_at": str(notice.created_at)
+           } for notice, user in notice_list],
+           "next_page": is_next
+        }
 
 
 @router.get("/notice/{notice_id}", status_code=status.HTTP_200_OK, tags=["notice"])
 async def get_notice_detail(notice_id: int, authorize: AuthJWT = Depends()):
-    token_check(authorize=authorize, type="access")
+    with session_scope() as session:
+        token_check(authorize=authorize, type="access")
 
-    is_notice(session=session, notice_id=notice_id)
-    notice, user = get_notice(session=session, notice_id=notice_id)
+        is_notice(session=session, notice_id=notice_id)
+        notice, user = get_notice(session=session, notice_id=notice_id)
 
-    return {
-       "notice": {
-           "name": user.name,
-           "created_at": str(notice.created_at),
-           "title": notice.title,
-           "content": notice.content
+        return {
+           "notice": {
+               "name": user.name,
+               "created_at": str(notice.created_at),
+               "title": notice.title,
+               "content": notice.content
+           }
        }
-   }
 
 
 @router.delete("/notice/{notice_id}", status_code=status.HTTP_200_OK, tags=["notice"])
 async def delete_one_notice(notice_id: int, authorize: AuthJWT = Depends()):
-    token_check(authorize=authorize, type="access")
+    with session_scope() as session:
+        token_check(authorize=authorize, type="access")
 
-    notice = is_notice(session=session, notice_id=notice_id, return_it=True)
+        notice = is_notice(session=session, notice_id=notice_id, return_it=True)
 
-    email = authorize.get_jwt_subject()
-    is_own_notice(notice=notice, email=email)
+        email = authorize.get_jwt_subject()
+        is_own_notice(notice=notice, email=email)
 
-    delete(session=session, del_thing=notice)
+        delete(session=session, del_thing=notice)
 
-    return {
-        "message": "success"
-    }
+        return {
+            "message": "success"
+        }
