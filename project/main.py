88959ca840ff8a0ff85d7562b000b2f837b87ce3
junkiYoon <juneki4933@gmail.com>
2021-08-17 19:02:51 +0900

    Update middleware
    create cors middleware

--- project/main.py
+++ project/main.py
@@ -1,15 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import uvicorn
 
 from project.apps import auth, notice
 
-from project.config import WORKERS
-
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router)
 app.include_router(notice.router)
 
 if __name__ == '__main__':
-    uvicorn.run("project.main:app", host="0.0.0.0", port=8000, workers=WORKERS)
+    uvicorn.run("project.main:app", host="0.0.0.0")
